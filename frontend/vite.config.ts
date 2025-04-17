d203a4d861b21423286e6ea391ae4e080679d49e
ShakthiSundar-K <shakthisundar01@gmail.com>
2025-04-17 06:15:45 +0530

    removed pwa setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,52 +1,7 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      devOptions: {
-        enabled: true,
-      },
-      strategies: "generateSW",
-      registerType: "autoUpdate",
-      
-      manifest: {
-        name: "Thandal Management APP",
-        short_name: "Thandal",
-        icons: [
-          {
-            "src": "manifest-icon-192.maskable.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "manifest-icon-192.maskable.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-          },
-          {
-            "src": "manifest-icon-512.maskable.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "manifest-icon-512.maskable.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-          }
-        ],
-        theme_color: "#1a1a1a",
-        background_color: "#1a1a1a",
-        start_url: "/",
-        display: "standalone",
-        orientation: "portrait",
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
